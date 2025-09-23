3d0da5e3b37136f67a81b4c33bc1a85779a198ee
Sotomedinayunior <39248241+Sotomedinayunior@users.noreply.github.com>
2025-09-23 10:41:56 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 // backend/api/index.js
 const app = require('../server');
 
-// Express app es un handler (req, res) => ...  ✔
-// Exporta función explícita para evitar dudas:
 module.exports = (req, res) => app(req, res);
